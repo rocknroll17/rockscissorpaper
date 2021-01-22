aabaa0e2e70e150bb1342e9738cbfec4e6f1da4d
rocknroll17 <53882578+rocknroll17@users.noreply.github.com>
2021-01-23 02:52:56 +0900

    1.0.0 First made version of this game.

---
+++ rock.py
@@ -0,0 +1,81 @@
+import random
+import os
+import time
+def game():
+    coin = 100
+    while 1:
+        print("코인수: ", coin)
+        try:
+            betting=int(input("coin을 얼마 거실지 정하세요: "))
+        except:
+            print("제대로 된 숫자를 입력하세요.")
+            continue
+        while betting > coin:
+            print("가진 코인보다 많은 코인을 베팅 거실 수는 없습니다.")
+            try:
+                betting=int(input("coin을 얼마 거실지 정하세요: "))
+            except:
+                continue
+        while betting == 0:
+            print("0개 이상의 코인을 겨셔야 합니다.")
+            try:
+                betting=int(input("coin을 얼마 거실지 정하세요: "))
+            except:
+                continue
+        if betting == coin:
+            print("올인하셨습니다.")
+
+        person=str(input("무엇을 낼지 정하세요(가위, 바위, 보):\n"))
+        while(person != "가위" and person != "바위" and person != "보"):
+            print("가위, 바위, 보 중에 하나만 낼 수 있습니다.")
+            person=str(input("무엇을 낼지 정하세요(가위, 바위, 보):\n"))
+            
+        machine = random.randint(1,100)
+        if person == "가위":
+            if machine > 45:
+                print("상대: 주먹")
+                wl = 0
+            elif machine <= 45:
+                print("상대: 보")
+                wl = 1
+            
+        if person == "바위":
+            if machine > 45:
+                print("상대: 보")
+                wl = 0
+            elif machine <= 45:
+                print("상대: 가위")
+                wl = 1
+            
+        if person == "보":
+            if machine > 45:
+                print("상대: 가위")
+                wl = 0
+            elif machine <= 45:
+                print("상대: 바위")
+                wl = 1
+
+        if wl == 1:
+            print("\n이겼습니다!!\n")
+            coin = coin + betting
+
+        elif wl == 0:
+            print("\n졌습니다..\n")
+            coin = coin - betting
+
+        time.sleep(1)
+        os.system('cls')
+        if coin == 0:
+            print("파산하셨습니다...")
+            ans = str(input("다시 하시겠습니까?(네 / 아니오): "))
+            while (ans != "네" and ans != "아니오"):
+                print("네 / 아니오 중에 하나만 입력하세요")
+                ans = str(input("다시 하시겠습니까?(네 / 아니오): "))
+            if ans == "네":
+                os.system('cls')
+                game()
+                return 0
+            elif ans == "아니오":
+                exit()
+
+game()
