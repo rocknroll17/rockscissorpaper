3576a74841300db01bcdcebe8fe5294660156e12
rocknroll17 <53882578+rocknroll17@users.noreply.github.com>
2021-01-23 14:59:15 +0900

    1.0.1 Changed percentage to variable, fixed some input for variety case.

--- rock.py
+++ rock.py
@@ -2,11 +2,13 @@ import random
 import os
 import time
 def game():
+    percent = 45
+    os.system('cls')
     coin = 100
     while 1:
-        print("코인수: ", coin)
+        print("코인수:", coin)
         try:
-            betting=int(input("coin을 얼마 거실지 정하세요: "))
+            betting=int(input("코인을 얼마 거실지 정하세요: "))
         except:
             print("제대로 된 숫자를 입력하세요.")
             continue
@@ -31,27 +33,27 @@ def game():
             person=str(input("무엇을 낼지 정하세요(가위, 바위, 보):\n"))
             
         machine = random.randint(1,100)
-        if person == "가위":
-            if machine > 45:
+        if person == "가위" or person == "S" or person == "s":
+            if machine > percent:
                 print("상대: 주먹")
                 wl = 0
-            elif machine <= 45:
+            elif machine <= percent:
                 print("상대: 보")
                 wl = 1
             
-        if person == "바위":
-            if machine > 45:
+        if person == "바위" or person == "R" or person == "r":
+            if machine > percent:
                 print("상대: 보")
                 wl = 0
-            elif machine <= 45:
+            elif machine <= percent:
                 print("상대: 가위")
                 wl = 1
             
-        if person == "보":
-            if machine > 45:
+        if person == "보" or person == "P" or person == "p":
+            if machine > percent:
                 print("상대: 가위")
                 wl = 0
-            elif machine <= 45:
+            elif machine <= percent:
                 print("상대: 바위")
                 wl = 1
 
@@ -68,14 +70,13 @@ def game():
         if coin == 0:
             print("파산하셨습니다...")
             ans = str(input("다시 하시겠습니까?(네 / 아니오): "))
-            while (ans != "네" and ans != "아니오"):
+            while (ans != "네" and ans != "Y" and ans != "YES" and ans != "yes" and ans != "아니오" and ans != "N" and ans != "NO" and ans != "no"):
                 print("네 / 아니오 중에 하나만 입력하세요")
                 ans = str(input("다시 하시겠습니까?(네 / 아니오): "))
-            if ans == "네":
-                os.system('cls')
+            if ans == "네" or ans == "Y" or ans == "YES" or ans == "yes":
                 game()
                 return 0
-            elif ans == "아니오":
+            elif ans == "아니오" or ans == "N" or ans == "NO" or ans == "no":
                 exit()
 
 game()
